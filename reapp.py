ef4299eaf6a5aea51bac8e8ce8f2184282745215
José Antonio Taquía Gutiérrez <jose_taquia@hotmail.com>
2021-07-01 11:38:39 -0500

    Update reapp.py

--- reapp.py
+++ reapp.py
@@ -30,7 +30,7 @@ def run_status():
 st.subheader('Ingeniería predictiva:: Predicción múltiples modelos')
 @st.cache
 def load_data():
-	df=pd.read_excel('D://CIS//Shiny//STREAMLIT//realestateapp_streamlit-master//data.xls')
+	df=pd.read_excel('data.xls')
 	df=df.drop(['country'],axis=1)
 	df=df[df['price']>0]
 	df.rename(columns={'statezip':'zip'}, inplace=True)
